12fb1ff5dcfbcc2bbbf8328350fc3719e8c8befa
Cherian Mathew <cmathew@chakravyu.org>
2017-03-22 11:21:11 +0100

    Ignore Int. test

--- org.dcs.nifi.repo/src/test/scala/org/dcs/nifi/repository/ProvenanceRepositorySpec.scala
+++ org.dcs.nifi.repo/src/test/scala/org/dcs/nifi/repository/ProvenanceRepositorySpec.scala
@@ -24,7 +24,7 @@ object ProvenanceRepositorySpec {
   val TestUpdatedAttributes = "updatedvattr1:value1,updatedattr2:value2"
 }
 
-//@Ignore // set to ignore until integration environment is setup
+@Ignore // set to ignore until integration environment is setup
 class ProvenanceRepositorySpec extends ProvenanceRepositoryBehaviors {
 
   val nifiPropertiesPath: Path = Paths.get(this.getClass.getResource("nifi.properties").toURI)
