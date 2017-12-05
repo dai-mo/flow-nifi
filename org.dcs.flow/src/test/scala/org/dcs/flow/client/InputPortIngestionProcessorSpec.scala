473fb3cb200a01daa2a49042031c4925ff59953a
Cherian Mathew <cmathew@chakravyu.org>
2017-12-05 14:06:31 +0100

    Propagate schema after schema property update, Remove unnecessary deletion of internal connections when deleting flow instance

--- org.dcs.flow/src/test/scala/org/dcs/flow/client/InputPortIngestionProcessorSpec.scala
+++ org.dcs.flow/src/test/scala/org/dcs/flow/client/InputPortIngestionProcessorSpec.scala
@@ -37,12 +37,10 @@ class InputPortIngestionProcessorSpec extends InputPortIngestionBehaviour {
 
 class InputPortIngestionProcessorISpec extends InputPortIngestionBehaviour {
   import InputPortIngestionSpec._
-  
+
+  ZkRemoteService.loadServiceCaches()
 
   "Creation / Deletion of Input Ingestion Processor" should "be valid" taggedAs IT in {
-    // FIXME: Bad Idea to initialise service caches in one (first) test
-    //        when running tests in parallel
-    ZkRemoteService.loadServiceCaches()
     var flowInstance = flowApi.create(FlowInstanceName, ClientId).futureValue
     var processor = validateCreateInputPortIngestionProcessor(processorApi, kaaPsd, flowInstance.id)
     flowInstance = validateFlowInstanceWithInputPortIngestionProcessor(flowApi, flowInstance.id, FlowInstanceName, 1)
@@ -71,17 +69,11 @@ class InputPortIngestionProcessorISpec extends InputPortIngestionBehaviour {
 
   // FIXME: This test requires the move of update properties logic from the web project to flow,
   //        so that we can resolve the read schema dynamically
-  "Instantiation / Lifecycle of Flow with an Input Ingestion Processor" should "be valid" taggedAs IT ignore {
+
+  "Instantiation / Lifecycle of Flow with an Input Ingestion Processor" should "be valid" taggedAs IT in {
     val flowTemplate = flowApi.templates().futureValue.find(_.name == InputPortIngestionFlowTemplateName).get
     val flowInstance = flowApi.instantiate(flowTemplate.id, ClientId).futureValue
-    validateFlowInstanceWithInputPortIngestionProcessor(flowApi, flowInstance.id, InputPortIngestionFlowTemplateName, 1)
-//    val kaaApplicationProperty = flowInstance.processors
-//      .find(_.name == "Kaa Ingestion Processor").get
-//        .propertyDefinitions
-      //.properties.find(_._1 == "kaa-application").get._2
-    flowApi.start(flowInstance.id, ClientId).futureValue
-    //Thread.sleep(5000)
-    flowApi.stop(flowInstance.id, ClientId).futureValue
+    validateFlowInstanceWithInputPortIngestionProcessor(flowApi, flowInstance.id, InputPortIngestionFlowTemplateName, 2)
     flowApi.remove(flowInstance.id, flowInstance.version, ClientId, true).map(deleteOk => assert(deleteOk))
   }
 
